be03a737775f4a92f016b44ebebd629c7d0d2e6e
Thomas Skerbis <thomas.skerbis@mac.com>
2020-11-03 15:31:15 +0100

    Create user_tracking_yform.php

---
+++ _docs/addons/ycom/user_tracking_yform.php
@@ -0,0 +1,36 @@
+---
+title: YCom-User in YForm Tabellen-History
+authors: [skerbis]
+prio:
+---
+
+# YCom-User in YForm Tabellen-History
+
+Für YCOM > 4.0.3 // oder aktuelle GitHub-Version
+
+Bei Aktionen im Frontend werden in der Tabellen-History von YForm die User aus YCom nicht erfasst. Stattdessen steht dort FrontendUser. Über den Extensionpoint `YCOM_HISTORY_USER` lässt sich der User überschreiben, der vom System vorgegeben ist. 
+
+- Eine Class im Ordner Lib des Project-AddOns anlegen. 
+
+`my_ycom.php`
+
+```php
+class my_ycom {
+    public static function getTheUser() {
+        $ycom_user = rex_ycom_auth::getUser();
+        $subject = rex_ycom_auth::getUser()->getValue('login');
+        return $subject ;
+    }
+}
+
+- In die boot.php folgenden Code einsetzen: 
+
+
+```php
+rex_extension::register('PACKAGES_INCLUDED', static function () {
+   if(!rex::isBackend() && rex_ycom_auth::getUser())
+   {
+   rex_extension::register('YCOM_HISTORY_USER', 'my_ycom::getTheUser');   
+   }
+});
+```
